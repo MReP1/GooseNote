c9fbb4ac21c9f5f918284b4c52d6ede9f3bae1c4
MReP1 <2351267654@qq.com>
2024-06-13 00:05:15 +0800

    support vibrate with iOS

--- shared/ui/src/iosMain/kotlin/Vibrator.ios.kt
+++ shared/ui/src/iosMain/kotlin/Vibrator.ios.kt
@@ -1,8 +1,16 @@
 import org.koin.core.module.Module
+import platform.UIKit.UIImpactFeedbackGenerator
+import platform.UIKit.UIImpactFeedbackStyle
 
 actual class Vibrator {
 
     actual fun vibrate(effect: Vibration) {
+        when (effect) {
+            Vibration.ClickShot,
+            is Vibration.OneShot -> {
+                UIImpactFeedbackGenerator(UIImpactFeedbackStyle.UIImpactFeedbackStyleMedium)
+            }
+        }
     }
 
 }
