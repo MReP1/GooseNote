062257813067fefee32a23420c5f5539ab6e3cb3
MReP1 <2351267654@qq.com>
2024-06-14 01:52:17 +0800

    code opt.

--- shared/ui/src/commonMain/kotlin/Vibrator.kt
+++ shared/ui/src/commonMain/kotlin/Vibrator.kt
@@ -1,5 +1,5 @@
 import androidx.compose.runtime.Composable
-import org.koin.compose.getKoin
+import org.koin.compose.koinInject
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -22,6 +22,4 @@ val vibratorModule = module { vibrator() }
 expect fun Module.vibrator()
 
 @Composable
-fun rememberVibrator(): Vibrator {
-    return getKoin().get()
-}
+fun rememberVibrator(): Vibrator = koinInject()
