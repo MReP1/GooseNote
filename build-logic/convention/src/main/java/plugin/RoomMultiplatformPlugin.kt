3647e29056faecc37aca2e73faf4c0b5649c4413
MReP1 <2351267654@qq.com>
2024-07-08 23:29:15 +0800

    Removed unnecessary import of `add` function.

--- build-logic/convention/src/main/java/plugin/RoomMultiplatformPlugin.kt
+++ build-logic/convention/src/main/java/plugin/RoomMultiplatformPlugin.kt
@@ -2,7 +2,6 @@ package plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
