ccb878093904400bbc2e772f4f66264fe84c85ba
MReP1 <2351267654@qq.com>
2024-09-12 22:35:51 +0800

    search bar.

--- shared/ui/src/commonMain/kotlin/little.goose.shared.ui/search/SearchScreen.kt
+++ shared/ui/src/commonMain/kotlin/little.goose.shared.ui/search/SearchScreen.kt
@@ -2,12 +2,17 @@ package little.goose.shared.ui.search
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Clear
@@ -17,9 +22,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
-import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -28,6 +34,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -41,52 +51,101 @@ fun SearchScreen(
     content: @Composable () -> Unit
 ) {
     val focusRequester = remember { FocusRequester() }
-    SearchBar(
-        modifier = modifier.focusRequester(focusRequester),
-        query = keyword,
-        onQueryChange = onKeywordChange,
-        onSearch = onKeywordChange,
-        active = true,
-        onActiveChange = {
-            if (!it) onBack()
-        },
-        leadingIcon = {
-            IconButton(onClick = onBack) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
-                    contentDescription = "Back"
-                )
+    val onActiveChange: (Boolean) -> Unit = {
+        if (!it) onBack()
+    }
+    val expanded = false
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Box(
+            modifier = modifier.semantics {
+                isTraversalGroup = true
             }
-        },
-        trailingIcon = {
-            if (keyword.isNotEmpty()) {
-                IconButton(onClick = { onKeywordChange("") }) {
-                    Icon(
-                        imageVector = Icons.Rounded.Clear,
-                        contentDescription = "Clear"
+        ) {
+            SearchBar(
+                inputField = {
+                    SearchBarDefaults.InputField(
+                        modifier = Modifier.focusRequester(focusRequester),
+                        query = keyword,
+                        onQueryChange = onKeywordChange,
+                        onSearch = onKeywordChange,
+                        expanded = expanded,
+                        onExpandedChange = onActiveChange,
+                        enabled = true,
+                        placeholder = null,
+                        leadingIcon = {
+                            IconButton(onClick = onBack) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                                    contentDescription = "Back"
+                                )
+                            }
+                        },
+                        trailingIcon = {
+                            if (keyword.isNotEmpty()) {
+                                IconButton(onClick = { onKeywordChange("") }) {
+                                    Icon(
+                                        imageVector = Icons.Rounded.Clear,
+                                        contentDescription = "Clear"
+                                    )
+                                }
+                            }
+                        },
+                        colors = SearchBarDefaults.inputFieldColors(),
+                        interactionSource = null,
+                    )
+                },
+                expanded = expanded,
+                onExpandedChange = onActiveChange,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .semantics { traversalIndex = 0F },
+                shape = SearchBarDefaults.inputFieldShape,
+                colors = SearchBarDefaults.colors(),
+                tonalElevation = SearchBarDefaults.TonalElevation,
+                shadowElevation = SearchBarDefaults.ShadowElevation,
+                windowInsets = SearchBarDefaults.windowInsets,
+                content = {
+                    // Suggestion
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .verticalScroll(rememberScrollState())
+                    ) {
+
+                    }
+                },
+            )
+
+            Box(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .windowInsetsPadding(
+                        WindowInsets.ime.union(BottomAppBarDefaults.windowInsets)
+                    )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .semantics { traversalIndex = 1F }
+                        .fillMaxSize()
+                        .padding(top = 72.dp)
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    content()
+                    SnackbarHost(
+                        hostState = snackbarHostState,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter)
                     )
                 }
             }
-        },
-        content = {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.background)
-                    .windowInsetsPadding(WindowInsets.ime.union(BottomAppBarDefaults.windowInsets))
-            ) {
-                content()
-                SnackbarHost(
-                    hostState = snackbarHostState,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.BottomCenter)
-                ) {
-                    Snackbar(snackbarData = it)
-                }
-            }
         }
-    )
+    }
+
+
     val keyboardController = LocalSoftwareKeyboardController.current
     LaunchedEffect(keyboardController) {
         delay(71)
